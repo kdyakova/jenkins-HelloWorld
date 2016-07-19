70293aae932c1e6838d0096588f77852f04d9e51
kriss95 <kristina.dqkova@gmail.com>
2016-07-19 11:24:03 +0300

    TestMethod12

--- HelloWorldTests/UnitTest1.cs
+++ HelloWorldTests/UnitTest1.cs
@@ -8,10 +8,8 @@ namespace HelloWorldTests
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestMethod12()
         {
-
-
             Assert.AreEqual("Hello World", Program.CreateMessage());
         }
     }
